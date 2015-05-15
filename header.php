f6071f69edb6d79e17eb77cdbc61472105903e3a
sofinapradhan <sofina12np@hotmail.com>
2015-05-15 13:11:43 -0600

    added files

---
+++ header.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+
+<title><?php echo($header_title);?></title>
+		<link href="style.css" type="text/css" rel="stylesheet">
+		<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</head>
+
+			<div id="titleheader">
+				<h1>Welcome to Travel Experts!&trade;</h1>
+			</div>
+
+			<div id="navBar">
+			<ul>
+			<li><a href="index.php" class="button">Home</a></li>
+            <li><a href="packages.php" class="button">Travel Packages</a></li>
+            <li><a href="register.php" class="button">Register</a></li>
+            <li><a href="about.php" class="button">About / FAQ</a></li>
+            <li><a href="contact.php" class="button">Contact</a></li>
+            </ul>
+			</div>
